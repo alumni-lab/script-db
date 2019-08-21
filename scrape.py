6fb786c699d64d54237608e5ac734ae5c1953060
Nima Boscarino <nima.boscarino@gmail.com>
2019-08-21 15:56:11 -0700

    get first quote by Gollum!

--- scrape.py
+++ scrape.py
@@ -1,8 +1,11 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 r = requests.get('https://www.imsdb.com/scripts/Lord-of-the-Rings-Fellowship-of-the-Ring,-The.html')
 text = r.text
 
 soup = BeautifulSoup(text, 'html.parser')
 
-print(soup.pre.text)
+quote = soup.find_all("b", text = re.compile('.*GOLLUM.*'))[0].nextSibling
+
+print(quote)
