b5475ace624c07db2c1ce01844ef4f3223d7f627
Won Seob Shin <wonseob07@gmail.com>
2019-08-21 16:42:01 -0700

    can select random lotr character and printou their first quote from the movie

---
+++ character.py
@@ -0,0 +1,26 @@
+import requests
+import re
+import random
+
+from bs4 import BeautifulSoup
+
+
+
+def randCharacter() :
+    imdb = requests.get('https://www.imdb.com/title/tt0120737/fullcredits?ref_=tt_cl_sm#cast')
+
+    text = imdb.text
+
+    soup = BeautifulSoup(text, 'html.parser')
+
+    characters = soup.find_all("td", {"class": "character"})
+    character_names = set()
+
+    for i in characters:
+        name = i.findChildren("a")
+        # print(name) 
+        if name :
+            character_names.add(name[0].text)
+            # print(name[0].text)
+
+    return random.choice(tuple(character_names))
--- scrape.py
+++ scrape.py
@@ -1,11 +1,15 @@
 import requests
 import re
+import character
+
 from bs4 import BeautifulSoup
 r = requests.get('https://www.imsdb.com/scripts/Lord-of-the-Rings-Fellowship-of-the-Ring,-The.html')
 text = r.text
 
 soup = BeautifulSoup(text, 'html.parser')
 
-quote = soup.find_all("b", text = re.compile('.*GOLLUM.*'))[0].nextSibling
+name = character.randCharacter().upper()
 
-print(quote)
+quote = soup.find_all("b", text = re.compile(f'.*{name}.*'))[0].nextSibling
+
+print(f'{name}: {quote}')
