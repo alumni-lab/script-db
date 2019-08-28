0aa313d47746514b2cb802bd4d590798b7eb0a1b
Won Seob Shin <wonseob07@gmail.com>
2019-08-28 15:19:29 -0700

    make dictionary quote for all characters

--- scrape.py
+++ scrape.py
@@ -10,14 +10,40 @@ quote_dictionary = {}
 
 soup = BeautifulSoup(text, 'html.parser')
 
+# get random character to pull quotes from
 names = character.randCharacter()
 
-for name in names :
-    if name :
-        quote_dictionary[name.upper()] = ["hello","world"]
 
-print(quote_dictionary)
+# print(quote_dictionary)
 
-quote = soup.find_all("b", text = re.compile(f'.*{name}.*'))[0].nextSibling
+
+
 
 # print(f'{name}: {quote}')
+
+
+
+for name in names :
+        if name :
+                quote_dictionary[name.upper()] = []
+                quotes = soup.find_all("b", text = re.compile(f'.*{name.upper()}.*'))
+                
+                for quote in quotes:
+                        quote_text = quote.nextSibling
+
+                        if quote_text.name == None :
+                                
+                                quote_text_split = quote_text.split('\n')
+                                # print(quote_text)
+                                quote_text_stripped = []
+
+                                for quote_text in quote_text_split :
+                                        quote_text_stripped.append(quote_text.strip())
+                                
+                                quote_text_joined = " ".join(quote_text_stripped)
+                                
+                                if quote_text_joined != " " :
+                                        quote_dictionary[name.upper()].append(quote_text_joined)
+                        
+
+print(quote_dictionary)
